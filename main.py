1a3ad5014695ca70484c1c02eedf942466a4c176
ZackK24 <zack.knudsen90@gmail.com>
2022-11-15 12:48:22 -0800

    Create the main.py file

---
+++ main.py
@@ -0,0 +1,19 @@
+# Sources and Overview of the Project.
+
+
+# Importing the libraries
+
+
+# Import the settings and the global variables needed
+
+
+# Analyze the pdf file which tells the info of houses and the different prices that they have
+
+
+# The code for the user to input what kind of house they are looking for
+
+
+# Will then take in the inputs for the user
+
+
+# Then will print out the user by predicting how much the user's house would be
