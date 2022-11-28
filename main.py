c9724af3484a2ea85b1feca7fee9c6d9e7f48d22
ZackK24 <zack.knudsen90@gmail.com>
2022-11-28 09:34:49 -0800

    Adding the Dataset and matplotlib

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ will be based on what kind of house you would like to have.
 
 Sources:
 https://www.w3schools.com/python/python_ml_linear_regression.asp
+https://colab.research.google.com/drive/1zk6NgYiR2UPzzmjzyhGbEduSt3gZHi7b
 '''
 
 
 # Importing the libraries
 import pandas as pd
+import matplotlib.pylot as plt
 import seaborn as sns
 
 # Import the settings and the global variables needed
-
-
+# This will create the datsheet for the machine. It will then read and analyze the excel file.
+# Then it will then show the data on a datasheet
+homesdata = pd.read_excel("HomePredictorData.slsx")
+homesdata.head()
 # Analyze the pdf file which tells the info of houses and the different prices that they have
 
 
