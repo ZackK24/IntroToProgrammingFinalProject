bc778dd95f5d3af1ae8cfed035729370d5f4e899
ZackK24 <zack.knudsen90@gmail.com>
2022-12-01 22:19:16 -0800

    Added the data File and a little training code

--- main.py
+++ main.py
@@ -11,16 +11,29 @@ https://colab.research.google.com/drive/1zk6NgYiR2UPzzmjzyhGbEduSt3gZHi7b
 
 # Importing the libraries
 import pandas as pd
-import matplotlib.pylot as plt
+import matplotlib as plt
 import seaborn as sns
-
+import numpy as np
+import openpyxl as py
+from sklearn.model_selection import train_test_split
 # Import the settings and the global variables needed
+
+
 # This will create the datsheet for the machine. It will then read and analyze the excel file.
 # Then it will then show the data on a datasheet
-homesdata = pd.read_csv("HousePricePrediction.xlsx")
-print(homesdata.head(5))
 # Analyze the pdf file which tells the info of houses and the different prices that they have
 
+homesdata = pd.read_excel("HomeData.xlsx")
+print(homesdata.head(16))
+
+# Train the model with the data given 
+# These will set the variables of what part of the data set I need.
+# 'Price' is in its own category since that is what the output would be.
+# Also set up the training and testing variables as well as telling how much of my data will be used for testing
+
+x = homesdata.drop(['Price'], axis = 1)
+y = homesdata(['Price'])
+X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size = 0.2) 
 
 # The code for the user to input what kind of house they are looking for
 
@@ -28,4 +41,4 @@ print(homesdata.head(5))
 # Will then take in the inputs for the user
 
 
-# Then will print out the user by predicting how much the user's house would be
+# Then will print out the outcome by predicting how much the user's house would be
