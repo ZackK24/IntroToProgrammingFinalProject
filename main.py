01c0a64e9ce729e73383c8f40dd5a0e7c79c09c6
ZackK24 <zack.knudsen90@gmail.com>
2022-12-07 23:03:47 -0800

    Added More Training Code

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 # Sources and Overview of the Project.
 '''
 This code will tell you what the projected price of a home in San Jose
@@ -26,7 +27,7 @@ from tkinter import ttk
 # Analyze the pdf file which tells the info of houses and the different prices that they have
 
 homesdata = pd.read_excel("HomeData.xlsx")
-print(homesdata.head(16))
+#print(homesdata.head)
 '''
 Train the model with the data given 
 These will set the variables of what part of the data set I need.
@@ -36,17 +37,19 @@ Also set up the training and testing variables as well as telling how much of my
 x = homesdata.drop(['Price'], axis = 1)
 y = homesdata['Price']
 X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size = 0.2) 
+# This will create the dat that we is used ot train the model where we need to use both the house's characteristics and their prices
 train_data = X_train.join(Y_train)
-train_data.hist(figsize = (15, 8))
-plt.figure(figsiz=(15, 8))
-sns.heatmap(train_data.cor(), annot = True, cmap="YlGnBu")
+# Will tell how varied each category is for the home data
+print(train_data.join(pd.get_dummies(train_data.Condition)).drop(['Condition'], axis = 1))
+# Will display a scatterplot of the correlation between Bath # and Bed #
+print(sns.scatterplot(x = 'Bed #', y = 'Bath #', data= train_data,))
 # Will draw the window using tkinter of the charts of data
 win = Tk()
 win.geometry("750x500")
 frame = LabelFrame(win, width= 400, height= 180, bd=5)
 entry = ttk.Entry(frame, width= 40)
 def chart():
-   message= "Your Charts "+ train_data.plt(figsize = (15, 8))
+   message= "Your Charts "+ train_data(figsize = (15, 8))
    label= Label(frame, text= message, font= ('Times New Roman', 14, 'italic'))
    label.pack(pady=30)
    entry.delete(0, 'end')
@@ -58,3 +61,4 @@ def chart():
 
 # Then will print out the outcome by predicting how much the user's house would be
 win.mainloop()
+
